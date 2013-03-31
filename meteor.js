1a0da34653599eb26f7e175aeb51369f22922aa6
Tom White <tom@sixdozen.com>
2013-03-30 18:26:41 -0700

    removed some code

--- meteor.js
+++ meteor.js
@@ -10,31 +10,6 @@ Game.allow({
 	return true;
     }
 });
-Frame = new Meteor.Collection('frame');
-Frame.allow({
-    insert: function (userId, z) {
-        return true;
-    },
-    remove: function (userId, z) {
-	return true;
-    },
-    update: function (userId, z) {
-	return true;
-    }
-});
-Frame2 = new Meteor.Collection('frame2');
-Frame2.allow({
-    insert: function (userId, z) {
-        return true;
-    },
-    remove: function (userId, z) {
-	return true;
-    },
-    update: function (userId, z) {
-	return true;
-    }
-});
-
 
 if (Meteor.isClient) {
     Meteor.startup(function () {
@@ -62,37 +37,12 @@ if (Meteor.isClient) {
         return grid += "</table>";
     }
 
-    Template.grid.gridMaker2 = function (a,b,classname) {
-        var grid = "<table class='"+classname+"'>";
-        for (var i = 0; i< a; i++) {
-            grid += "<tr>";
-            for (var j=0; j<b; j++)
-            {
-                var id = i+"-"+j;
-                grid += "<td id='"+id+"' class='"+Template.grid.style(id)+"' style='background-color:"+Template.grid.color2(id)+"'></td>"
-            }
-            grid += "</tr>";
-        }
-        return grid += "</table>";
-    }
-
-
-    Template.grid.allgrids = function() {
-        str = "";
-        Frame.find({},{$sort:{time:-1}}).fetch().forEach(function(z) { str += z.index; });
-        return str;
-    };
-    
-
     Template.grid.style = function(id) {
         return Game.find({index:id}).fetch().length ? 'selected' : '';
     }
 
     Template.grid.color = function(id) {
         return Game.findOne({index:id}) ? Game.findOne({index:id}).color : 'white';
-    }
-    Template.grid.color2 = function(id) {
-        return (f = Frame2.findOne({index:id},{sort:{time:-1}})) ? f.color : 'white';
     }
 
     var handle = null;
@@ -102,10 +52,8 @@ if (Meteor.isClient) {
             var time = new Date().getTime();
             if(Game.find({index:it.id}).fetch().length) {
                 Meteor.call('remove',it.id);
-                Frame.insert({index:it.id,color:'#fff',time:time});
             } else {
                 Game.insert({index:it.id,color:Session.get('mycolor'),time:time});
-                Frame.insert({index:it.id,color:Session.get('mycolor'),time:time});
             }            
         },
         'click #clear' : function(e) {
@@ -113,37 +61,7 @@ if (Meteor.isClient) {
         },
         'click #clearmemory' : function(e) {
             Meteor.call("clearframe2");
-        },
-        'click #save' : function(e) {
-            Meteor.call("save");
-        },
-        'click #stop' : function(e) {
-            Session.set('showallgrid',false);
-        },
-        'click #play' : function(e) {
-            Session.set('showallgrid',true);
-            Meteor.call("clearframe2");
-            if (handle) {
-                console.log(handle);
-                Meteor.clearInterval(handle);
-            }
-            var frames = Frame.find({},{$sort:{time:-1}}).fetch()
-            var i = 0;
-            handle = Meteor.setInterval(
-                function(){             
-                    if(i == frames.length) {
-                        Meteor.call("clearframe2");
-                        i = 0; 
-                    }                  
-                    
-                    console.log(frames[i].index+" "+i+" "+frames.length);
-                    Frame2.insert(frames[i]);
-                    i++;
-                }
-                , 100);
-            
         }
-
     });
     
 }
@@ -153,22 +71,12 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
    
-    Meteor.methods({
-        'clear' : function() {
-            Game.remove({});
-        },
-        'clearframe2' : function() {
-            Frame2.remove({});
-        },
-        'remove' : function(id) {
-            Game.remove({index:id});
-        
-        },
-        'save' : function() {
-            Frame.insert(Game.find({}).fetch());            
-        },
-        'clearmemory' : function() {
-            Frame.remove({});            
-        }
-    });
+  Meteor.methods({
+      'clear' : function() {
+          Game.remove({});
+      },
+      'remove' : function(id) {
+          Game.remove({index:id});        
+      }
+  });
 }
